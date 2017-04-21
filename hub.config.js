f7cc88e4afb59c4b5fc946879269e0a8ecb091a4
george francis <georgefrancis105@gmail.com>
2017-04-22 11:35:27 +1200

    Fetch hub object

---
+++ hub.config.js
@@ -0,0 +1,7 @@
+var hubName = 'raspberry pi 1';
+var hubKey = 'QqWrI4d55rmb42YWSAT2qI381ZLj8H1h';
+
+module.exports = {
+    hubName,
+    hubKey
+}
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("firebase-admin.config.json");
+var admin = require('firebase-admin');
+var hubKey = require('./hub.config').hubKey;
+var serviceAccount = require('./firebase-admin.config.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://angle-control.firebaseio.com"
+  databaseURL: 'https://angle-control.firebaseio.com'
 });
 
+console.log(hubKey);
+
+var db = admin.database();
+var hubRef = db.ref("hubs/" + hubKey);
+var deviceRefs = [];
+
+hubRef.on('value', (hub) => {
+  console.log('hub', hub.val());
+}, (err) => {
+  console.log('DeviceConnected update error:', err);
+});
